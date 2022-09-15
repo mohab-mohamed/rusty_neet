937090cbde176deb18928cc4003bcbdef9097789
Mohab <23743556+mohab-mohamed@users.noreply.github.com>
2022-09-14 22:35:16 -0400

    $implement valid_anagram

--- src/arrays_and_hashing/valid_anagram.rs
+++ src/arrays_and_hashing/valid_anagram.rs
@@ -1,9 +1,31 @@
 use std::collections::HashMap;
 
 pub fn valid_anagram(s: &str, t: &str) -> bool {
-    let mut char_map: HashMap<char, u32> = HashMap::new();
+    if s.chars().count() != t.chars().count() {
+        return false;
+    }
 
-    false
+    let mut s_map: HashMap<char, u32> = HashMap::new();
+    let mut t_map: HashMap<char, u32> = HashMap::new();
+    for char in s.chars() {
+        let count = s_map.entry(char).or_insert(0);
+        *count += 1;
+    }
+    for char in t.chars() {
+        let count = t_map.entry(char).or_insert(0);
+        *count += 1;
+    }
+
+    if s_map.len() != t_map.len() {
+        return false;
+    }
+    for (key, _) in &s_map {
+        if s_map.get(&key) != t_map.get(&key) {
+            return false;
+        }
+    }
+
+    true
 }
 
 #[cfg(test)]
@@ -21,7 +43,7 @@ mod tests {
     #[test]
     fn not_anagram() {
         let mock_s = "anagram";
-        let mock_t = "nagaram";
+        let mock_t = "uagaram";
         let result = valid_anagram(mock_s, mock_t);
         assert_eq!(result, false);
     }
